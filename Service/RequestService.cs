fb7f205373fd146a75fd84ec574c362e260500dd
Game Game <game48875@gmail.com>
2022-06-27 17:09:42 +0800

    Sort

--- Service/RequestService.cs
+++ Service/RequestService.cs
@@ -14,26 +14,14 @@ namespace WebApplication1.Service
         public string Get(string api, string method){
             // Create a request for the URL.
             WebRequest request = WebRequest.Create(url + api + "/" + method);
-            // If required by the server, set the credentials.
             request.Credentials = CredentialCache.DefaultCredentials;
-            // Get the response.
             WebResponse response = request.GetResponse();
-            // Display the status.
-            // Console.WriteLine(((HttpWebResponse)response).StatusDescription);
-
-            // Get the stream containing content returned by the server.
-            // The using block ensures the stream is automatically closed.
             string s = "";
             using (Stream dataStream = response.GetResponseStream())
             {
-                // Open the stream using a StreamReader for easy access.
                 StreamReader reader = new StreamReader(dataStream);
-                // Read the content.
                 s = reader.ReadToEnd();
-                // Display the content.
-                // Console.WriteLine(responseFromServer);
             }
-            // Close the response.
             response.Close();
             Console.WriteLine($"{api}/{method}");
             return s;
@@ -55,14 +43,9 @@ namespace WebApplication1.Service
             string s = "";
             using (Stream dataStream = response.GetResponseStream())
             {
-                // Open the stream using a StreamReader for easy access.
                 StreamReader reader = new StreamReader(dataStream);
-                // Read the content.
                 s = reader.ReadToEnd();
-                // Display the content.
-                // Console.WriteLine(responseFromServer);
             }
-            // Close the response.
             response.Close();
             return s;
         }
